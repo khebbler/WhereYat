89c0d086134d318ec276338c4f78eb79855df1e1
Katherine <khebbler@gmail.com>
2024-11-14 16:24:55 -0600

    displayTweets function

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 $(document).ready(() => {
-  const $body = $('#body');
-  $body.html('');
+  // $tweetFeed = #tweet-feed
+  const $tweetFeed = $('#tweet-feed');
 
+  const displayTweets = () => {
+    // clears #tweet-feed container
+    $tweetFeed.empty();
+  // looping through tweets & mapping
   const $tweets = streams.home.map((tweet) => {
+    // $tweet = new div element
     const $tweet = $('<div class="list-group-item"></div>'); // bootstrap
+    // text = username : message
     const text = `@${tweet.user}: ${tweet.message}`;
-
+    // setting $tweet to text
     $tweet.text(text);
-
+    // return array of $tweet elements
     return $tweet;
   });
-  $body.append($tweets);
-
+  $tweetFeed.append($tweets);
+};
+// calling displayTweets to display tweets
+displayTweets();
 });
-
