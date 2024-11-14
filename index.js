6b2869fa98545487054996b7ff52e510834039d7
Katherine <khebbler@gmail.com>
2024-11-14 15:56:08 -0600

    Added header & tweet box

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 $(document).ready(() => {
-  const $body = $('body');
-  $body.html(''); // .html('') clears everything inside the tag
+  const $body = $('#body');
+  $body.html('');
 
   const $tweets = streams.home.map((tweet) => {
-    const $tweet = $('<div></div>');
+    const $tweet = $('<div class="list-group-item"></div>'); // bootstrap
     const text = `@${tweet.user}: ${tweet.message}`;
 
     $tweet.text(text);
