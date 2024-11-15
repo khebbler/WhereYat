8f6d8722050f3869e6e4a5830de6961a89a5870e
Katherine <khebbler@gmail.com>
2024-11-15 17:09:03 -0600

    Added moments.js

--- index.js
+++ index.js
@@ -2,22 +2,43 @@ $(document).ready(() => {
   // $tweetFeed = #tweet-feed
   const $tweetFeed = $('#tweet-feed');
 
+  // displayTweets
   const displayTweets = () => {
     // clears #tweet-feed container
     $tweetFeed.empty();
-  // looping through tweets & mapping
+
+  // looping through tweets in streams.home & mapping
   const $tweets = streams.home.map((tweet) => {
+
     // $tweet = new div element
     const $tweet = $('<div class="list-group-item"></div>'); // bootstrap
-    // text = username : message
-    const text = `@${tweet.user}: ${tweet.message}`;
-    // setting $tweet to text
-    $tweet.text(text);
-    // return array of $tweet elements
+
+    // $timestamp
+    const timestamp = moment(tweet.created_at).format('MMMM D, YYYY h:mm A'); // moments.js
+
+    // HTML for tweet
+    const tweetHtml = `
+      <div>
+        <strong>@${tweet.user}</strong>
+        <span class="text-muted" style="float: right;">${timestamp}</span>
+      </div>
+      <p>${tweet.message}</p>
+    `;
+
+    // adding HTML to $tweet
+    $tweet.html(tweetHtml);
+
+    // returning $tweet
     return $tweet;
+
+
   });
+  // appending HTML to $tweetFeed
   $tweetFeed.append($tweets);
+
 };
-// calling displayTweets to display tweets
-displayTweets();
+
+  // calling displayTweets to display tweets
+  displayTweets();
 });
+
