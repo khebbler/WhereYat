e4a2853064175ef4475f44e3353a0864259e419f
Katherine <khebbler@gmail.com>
2024-11-17 16:26:20 -0600

    Added hashtag click event listener

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+// DOM
 $(document).ready(() => {
+  
   // feed of tweets
   const $tweetFeed = $('#tweet-feed');
 
   // input for new tweets
   const $newTweet = $('#new-tweet');
 
-  // submit tweet button
+  // tweet button
   const $tweetButton = $('#tweet-button');
 
   // back to home button
-  const $backButton = $('<button class="btn btn-secondary mt-2 d-none">Back to Home</button>');
+  const $backButton = $('<button class="btn btn-secondary mt-2 d-none">Back to Home</button>'); // (d-none hides button)
 
   // adding back button
   $tweetFeed.before($backButton);
@@ -20,22 +22,31 @@ $(document).ready(() => {
 
   // displayTweets
   const displayTweets = (tweets = streams.home) => {
+
     // clearing tweetFeed
     $tweetFeed.empty();
 
-  // reversing tweets before mapping
-  const $tweets = tweets.slice().reverse().map((tweet) => {
+    // reversing tweets before mapping
+    const $tweets = tweets.slice().reverse().map((tweet) => {
 
-    // $tweet = new div element
-    const $tweet = $('<div class="list-group-item"></div>'); // bootstrap
+      // $tweet = new div element
+      const $tweet = $('<div class="list-group-item"></div>'); // bootstrap
 
-    // timestamp of tweet
-    const timestamp = moment(tweet.created_at).format('MMMM D, YYYY h:mm A'); // moments.js
+      // timestamp of tweet
+      const timestamp = moment(tweet.created_at).format('MMMM D, YYYY h:mm A'); // moments.js
 
-    // timestamp of how long ago tweet was created
-    const createdTimestamp = moment(tweet.created_at).fromNow(); // moment.js
+      // timestamp of how long ago tweet was created
+      const createdTimestamp = moment(tweet.created_at).fromNow(); // moment.js
+
+    // using regex to find hashtags
+    const hashtag = tweet.message.replace(/#(\w+)/g, (match, tag) => {
+      // making hashtags clickable
+      return `<span class="hashtag text-info" data-hashtag="${tag}" style="cursor: pointer;">${match}</span>`;
+    });
+
 
     // html for tweet
+        // username, timestamp, hashtag
     const tweetHtml = `
       <div>
         <strong class="username" data-username="${tweet.user}" style="cursor: pointer;">
@@ -45,7 +56,7 @@ $(document).ready(() => {
             ${timestamp} (${createdTimestamp})
         </span>
       </div>
-      <p>${tweet.message}</p>
+      <p>${hashtag}</p>
     `;
 
     // adding html to tweet
@@ -88,6 +99,19 @@ $(document).ready(() => {
     }
   });
 
+  // Click event listener for hashtags
+  $tweetFeed.on('click', '.hashtag', function () {
+    // getting clicked hashtag
+    const hashtag = $(this).data('hashtag');
+    // filtering tweets with hashtag
+    const hashtagTweets = streams.home.filter((tweet) => tweet.message.includes(`#${hashtag}`));
+    // showing filtered tweets
+    displayTweets(hashtagTweets);
+    $backButton.removeClass('d-none');
+  });
+
+  // console.log(hashtag);
+
 // back to home click event listener
 $backButton.on('click', () => {
   // showing all tweets
@@ -102,7 +126,7 @@ $tweetButton.on('click', () => {
   const message = $newTweet.val().trim();
 
   if (!window.visitor) {
-    alert('Please set your username before tweeting!');
+    alert('Error');
     return;
   }
 
