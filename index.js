c73bf4b6e0a79df8cef73a3733090903f7f083eb
Katherine <khebbler@gmail.com>
2024-11-17 16:38:35 -0600

    Fixed formatting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // DOM
 $(document).ready(() => {
-  
+
   // feed of tweets
   const $tweetFeed = $('#tweet-feed');
 
@@ -38,64 +38,57 @@ $(document).ready(() => {
       // timestamp of how long ago tweet was created
       const createdTimestamp = moment(tweet.created_at).fromNow(); // moment.js
 
-    // using regex to find hashtags
-    const hashtag = tweet.message.replace(/#(\w+)/g, (match, tag) => {
-      // making hashtags clickable
-      return `<span class="hashtag text-info" data-hashtag="${tag}" style="cursor: pointer;">${match}</span>`;
+      // using regex to find hashtags
+      const hashtag = tweet.message.replace(/#(\w+)/g, (match, tag) => {
+        // making hashtags clickable
+        return `<span class="hashtag text-info" data-hashtag="${tag}" style="cursor: pointer;">${match}</span>`;
+      });
+
+      // html for tweet
+        // username, timestamp, hashtag
+      const tweetHtml = `
+        <div>
+          <strong class="username" data-username="${tweet.user}" style="cursor: pointer;">
+              @${tweet.user}
+            </strong>
+            <span class="text-muted float-end">
+              ${timestamp} (${createdTimestamp})
+          </span>
+        </div>
+        <p>${hashtag}</p>
+      `;
+
+      // adding html to tweet
+      $tweet.html(tweetHtml);
+
+      // returning tweet
+      return $tweet;
     });
 
-
-    // html for tweet
-        // username, timestamp, hashtag
-    const tweetHtml = `
-      <div>
-        <strong class="username" data-username="${tweet.user}" style="cursor: pointer;">
-            @${tweet.user}
-          </strong>
-          <span class="text-muted float-end">
-            ${timestamp} (${createdTimestamp})
-        </span>
-      </div>
-      <p>${hashtag}</p>
-    `;
-
-    // adding html to tweet
-    $tweet.html(tweetHtml);
-
-    // returning tweet
-    return $tweet;
-  });
-
-  // appending HTML to $tweetFeed
-  $tweetFeed.append($tweets);
-};
+    // appending HTML to $tweetFeed
+    $tweetFeed.append($tweets);
+  };
 
   // username click event listener
   $tweetFeed.on('click', '.username', function ()  {
 
     // getting clicked username
     const username = $(this).data('username');
-
-    console.log(username);
     console.log(this);
     console.log(username);
 
     // getting users tweets
     const userTweets = streams.users[username];
-
     console.log(username, userTweets);
 
     // checking is user exist
     if (username && streams.users[username]) {
-      // getting user's tweets
-      const userTweets = streams.users[username];
       // showing user's tweets
       displayTweets(userTweets);
       // showing back button
       $backButton.removeClass('d-none'); // bootstrap
     } else {
       console.error('No tweets');
-
     }
   });
 
@@ -112,40 +105,40 @@ $(document).ready(() => {
 
   // console.log(hashtag);
 
-// back to home click event listener
-$backButton.on('click', () => {
-  // showing all tweets
-  displayTweets();
-  // hiding back button
-  $backButton.addClass('d-none'); // bootstrap
-});
+  // back to home click event listener
+  $backButton.on('click', () => {
+    // showing all tweets
+    displayTweets();
+    // hiding back button
+    $backButton.addClass('d-none'); // bootstrap
+  });
 
-// tweet button click event listener
-$tweetButton.on('click', () => {
-  // getting input value
-  const message = $newTweet.val().trim();
+  // tweet button click event listener
+  $tweetButton.on('click', () => {
+    // getting input value
+    const message = $newTweet.val().trim();
 
-  if (!window.visitor) {
-    alert('Error');
-    return;
-  }
-
-  if (message) {
-    // checking if visitor is in streams.users
-    if (!streams.users[window.visitor]) {
-      streams.users[window.visitor] = [];
+    if (!window.visitor) {
+      alert('Error');
+      return;
     }
 
-    // adding tweet
-    writeTweet(message);
-    // clearing input box
-    $newTweet.val('');
-    // refreshing tweet feed
-    displayTweets();
-  } else {
-    alert('Say something!');
-  }
-});
+    if (message) {
+      // checking if visitor is in streams.users
+      if (!streams.users[window.visitor]) {
+        // user's tweets array
+        streams.users[window.visitor] = [];
+      }
+      // adding tweet
+      writeTweet(message);
+      // clearing input box
+      $newTweet.val('');
+      // refreshing tweet feed
+     displayTweets();
+    } else {
+      alert('Say something!');
+    }
+  });
 
   // calling displayTweets to show tweets
   displayTweets();
